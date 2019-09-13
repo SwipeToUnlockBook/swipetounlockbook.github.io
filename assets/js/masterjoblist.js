fde904589af9e1e90e7dd96376874c05b84870ee
Neel Mehta <neelmehta96@gmail.com>
2019-09-13 12:38:37 -0400

    start filter code

---
+++ assets/js/masterjoblist.js
@@ -0,0 +1,25 @@
+/**
+  This script runs on the Master Job List page
+*/
+$(function(){
+});
+
+/**
+  Apply a filter to all jobs based on a certain attribte.
+  Available attributes:
+  - `type`: "PM", "Sales", etc.
+  - `technical`: "0", "1"
+  - `rotational`: "0", "1"
+
+  TODO: support multiple filters
+*/
+function filterJobsByAttribute(attribute, value){
+  // hide all jobs where its `attribute` is not `value`, and show the rest
+  $('.job-block').toggle(function(){
+    return $(this).data(attribute) === value
+  });
+}
+
+function resetFilters(){
+  $('.job-block').show()
+}
